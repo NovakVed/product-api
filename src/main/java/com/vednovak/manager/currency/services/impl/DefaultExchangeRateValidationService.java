4227f3a58345010ba6db1f0fee151e3f3642f077
NovakVed <vednovak@gmail.com>
2025-04-13 00:56:25 +0200

    fix: instead of throwing ex, log invalid currencies

--- src/main/java/com/vednovak/manager/currency/services/impl/DefaultExchangeRateValidationService.java
+++ src/main/java/com/vednovak/manager/currency/services/impl/DefaultExchangeRateValidationService.java
@@ -3,6 +3,7 @@ package com.vednovak.manager.currency.services.impl;
 import com.vednovak.manager.currency.data.dtos.CurrencyExchangeRateData;
 import com.vednovak.manager.currency.exceptions.CurrencyExchangeRateException;
 import com.vednovak.manager.currency.services.ExchangeRateValidationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @PropertySource("classpath:currencies.properties")
 public class DefaultExchangeRateValidationService implements ExchangeRateValidationService {
@@ -23,20 +25,27 @@ public class DefaultExchangeRateValidationService implements ExchangeRateValidat
 
     @Override
     public void validateFetchedExchangeRates(Set<CurrencyExchangeRateData> fetchedExchangeRates) {
-        if (fetchedExchangeRates == null
-                || fetchedExchangeRates.isEmpty()
-                || fetchedExchangeRates.size() != supportedCurrencies.size()
-                || !areAllSupportedCurrenciesPresent(fetchedExchangeRates, supportedCurrencies)) {
+        if (fetchedExchangeRates == null || fetchedExchangeRates.isEmpty()) {
             throw new CurrencyExchangeRateException("Invalid fetched exchange rates or supported currencies.");
+        }
+
+        if (fetchedExchangeRates.size() != supportedCurrencies.size()) {
+            logMissingCurrencies(fetchedExchangeRates);
         }
     }
 
-    private boolean areAllSupportedCurrenciesPresent(
-            Set<CurrencyExchangeRateData> fetchedExchangeRates,
-            Set<String> supportedCurrencies) {
-        return fetchedExchangeRates.stream()
+    private void logMissingCurrencies(Set<CurrencyExchangeRateData> fetchedExchangeRates) {
+        Set<String> fetchedCurrencies = fetchedExchangeRates.stream()
                 .map(CurrencyExchangeRateData::getCurrency)
-                .collect(Collectors.toSet())
-                .containsAll(supportedCurrencies);
+                .collect(Collectors.toSet());
+
+        Set<String> missingCurrencies = supportedCurrencies.stream()
+                .filter(currency -> !fetchedCurrencies.contains(currency))
+                .collect(Collectors.toSet());
+
+        if (!missingCurrencies.isEmpty()) {
+            log.warn("The following supported currencies are missing from the fetched exchange rates: {}",
+                    missingCurrencies);
+        }
     }
 }
