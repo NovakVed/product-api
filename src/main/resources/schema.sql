ccc952d6784c5467a7082f7474fbec416d46884a
NovakVed <vednovak@gmail.com>
2025-04-12 22:58:16 +0200

    feat: add indexes in db

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,19 @@
 DROP TABLE IF EXISTS products CASCADE;
+DROP INDEX IF EXISTS idx_products_code;
+DROP INDEX IF EXISTS idx_products_id;
 
 CREATE TABLE products
 (
-    id           SERIAL PRIMARY KEY    NOT NULL,
-    code         VARCHAR(10)           NOT NULL UNIQUE,
-    name         VARCHAR(255)          NOT NULL, -- TODO add localization?
-    price_eur    NUMERIC(38, 2)        NOT NULL, -- TODO is check needed if you will do the same via code?
-    price_usd    NUMERIC(38, 2)        NOT NULL, -- TODO maybe get the latest value, cache it and then serve this cache in response?
-    is_available BOOLEAN DEFAULT FALSE NOT NULL
--- TODO: add creation time
---  add last modification time?
+    id            SERIAL PRIMARY KEY                  NOT NULL,
+    code          VARCHAR(10)                         NOT NULL UNIQUE,
+    name          VARCHAR(255)                        NOT NULL, -- TODO add localization?
+    price_eur     NUMERIC(38, 2)                      NOT NULL, -- TODO is check needed if you will do the same via code?
+    -- TODO check if 38 is too much?
+    is_available  BOOLEAN   DEFAULT FALSE             NOT NULL,
+    creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+-- TODO: what is pk in db, is this used everywhere?, pk vs id?
+-- TODO: create index for code, check what is index as well!
 );
+
+CREATE INDEX idx_products_id ON products (id);
+CREATE INDEX idx_products_code ON products (code);
