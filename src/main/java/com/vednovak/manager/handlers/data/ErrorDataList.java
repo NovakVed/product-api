65542f49a906af7b0ee1ebeb6871b7a15b82b7c7
NovakVed <vednovak@gmail.com>
2025-04-14 23:17:39 +0200

    chore: improve OpenAPI

--- src/main/java/com/vednovak/manager/handlers/data/ErrorDataList.java
+++ src/main/java/com/vednovak/manager/handlers/data/ErrorDataList.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
+@Schema(description = "Represents the response containing list of error messages")
 public record ErrorDataList(@Schema(description = "List of errors") List<ErrorData> errors) {
 
     public static ErrorDataList forErrors(List<ErrorData> errors) {
