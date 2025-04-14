4ab7452b1a29a39455481d1031f8dfb7a1eaed98
NovakVed <vednovak@gmail.com>
2025-04-14 18:45:32 +0200

    test: add mock test for CurrencyExchangeRateScheduler

---
+++ src/test/java/com/vednovak/manager/currency/schedulers/CurrencyExchangeRateSchedulerTest.java
@@ -0,0 +1,36 @@
+package com.vednovak.manager.currency.schedulers;
+
+import com.vednovak.manager.currency.services.CurrencyExchangeRateService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CurrencyExchangeRateSchedulerTest {
+
+    @MockitoBean
+    private CurrencyExchangeRateService currencyExchangeRateService;
+
+    @Test
+    @DisplayName("When updateExchangeRates is called, then CurrencyExchangeRateService updates exchange rates")
+    void whenUpdateExchangeRatesCalled_ThenServiceUpdatesExchangeRates() {
+        new CurrencyExchangeRateScheduler(currencyExchangeRateService).updateExchangeRates();
+
+        verify(currencyExchangeRateService, times(1)).updateExchangeRates();
+    }
+
+    @Test
+    @DisplayName("When updateExchangeRates throws exception, then it is logged without propagation")
+    void whenUpdateExchangeRatesThrowsException_ThenLogError() {
+        doThrow(new RuntimeException("Test exception")).when(currencyExchangeRateService).updateExchangeRates();
+
+        new CurrencyExchangeRateScheduler(currencyExchangeRateService).updateExchangeRates();
+
+        verify(currencyExchangeRateService, times(1)).updateExchangeRates();
+    }
+}
